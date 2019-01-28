da3112292296a8f741a01453fa8c6c6b9cd7dd8c
mithileshtarkar <mithilesh.tarkar@ves.ac.in>
2019-01-28 22:58:46 +0530

    created the skeleton of the admin app in 4 different components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { AsideComponent } from './aside/aside.component';
+import { AppFooterComponent } from './app-footer/app-footer.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AsideComponent,
+    AppFooterComponent,
+    DashboardComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule
